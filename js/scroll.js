0965ff08269d96f18d90f06eeeda2abe494a831b
chiazo <chiazo@live.unc.edu>
2020-03-13 11:20:42 -0500

    added sticky nav bar and js file

---
+++ js/scroll.js
@@ -0,0 +1,29 @@
+let navLinks = document.querySelectorAll("nav ul li a");
+let landingSections = document.querySelectorAll("main section");
+
+let lastId;
+let curr = [];
+
+window.addEventListener("scroll", e => {
+    let fromTop = window.scrollY;
+    console.log(fromTop);
+
+    navLinks.forEach(link => {
+        let section = document.querySelector(link.hash);
+        if (fromTop <= 96) {
+            document.getElementById("navigation").classList.remove("on-scroll");
+        } else {
+            document.getElementById("navigation").classList.add("on-scroll");
+        }
+        if (
+            section.offsetTop <= fromTop && 
+            section.offsetTop + section.offsetHeight > fromTop
+        ) 
+        {
+            
+            link.classList.add("current");
+        } else {
+            link.classList.remove("current");
+        }
+    });
+});
